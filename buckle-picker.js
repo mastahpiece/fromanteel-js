db231b31cd6286c720fade7e1450648e0c5c0cd3
mastahpiece <m_s_abdoel@hotmail.com>
2023-02-12 13:42:26 +0100

    fix

--- buckle-picker.js
+++ buckle-picker.js
@@ -18,7 +18,7 @@ getShopifyProducts().then((data) => {
               <option id="buckleOk" value="1" data-money-convertible>${data?.options.filter(i => i.yes)[0].yes}</option>
               
           </select>
-          <svg style="height: 15px; width: 15px;" class="Icon Icon--select-arrow" role="presentation" viewBox="0 0 19 12">
+          <svg class="Icon Icon--select-arrow" role="presentation" viewBox="0 0 19 12">
               <polyline fill="none" stroke="currentColor" points="17 2 9.5 10 2 2" fill-rule="evenodd" stroke-width="2" stroke-linecap="square"></polyline>
           </svg>
   
@@ -26,7 +26,7 @@ getShopifyProducts().then((data) => {
       <header class="popTitle --toggleHeight"></header>
       <div class="buckle__popover__content">
       <button type="button" class="buckle__popCloseButton">
-      <svg class="close-popup-icon"version="1.1" viewBox="0 0 512 512"xml:space="preserve"><path d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"/></svg>
+      <svg style="height: 15px; width: 15px;" class="close-popup-icon"version="1.1" viewBox="0 0 512 512"xml:space="preserve"><path d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"/></svg>
       </button>
       <header class="popTitle"><span id="popMsg" class="popover__message">${data?.popupTitle}</span></header>
       <div class="content">
